e9777b07ebca7ce4b53ce579762ff4b1af005e64
Tony Dao <tonydao@crc-dot1x-nat-10-239-22-16.bu.edu>
2024-02-21 16:42:47 -0500

    finished landing page

--- app/op-next/pages/index.tsx
+++ app/op-next/pages/index.tsx
@@ -1,7 +1,29 @@
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { Open_Sans } from 'next/font/google'
+
+const openSansNormal = Open_Sans({ weight: "400", style: 'normal', subsets: ['latin'] });
+
+const openSansBold = Open_Sans({ weight: "700", style: 'normal', subsets: ['latin'] });
+
+
 export default function Page () {
   return (
-    <div>
-      <h1>OpenPoll</h1>
+    <div className={styles.container}> 
+      <Image
+        src="/OpenPollLogo1.png"
+        alt="Open Poll Logo"
+        width={300}
+        height={300}
+        className={styles.logo}
+      />
+      <div className={styles.text}>
+        <h1 className={openSansBold.className}>Log in to Open Poll</h1>
+        <p className={openSansNormal.className}>Welcome back! Please log in below</p>
+      </div>
+      <button className={styles.loginButton}>
+        Sign in with Google
+      </button>
     </div>
-  )
+  );
 }
