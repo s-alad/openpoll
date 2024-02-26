9633fe258752b91e505058d39ae5a20720e2ff80
s-alad <n7.saad@gmail.com>
2024-02-26 04:09:57 -0500

    redir

--- app/op-next/pages/_app.tsx
+++ app/op-next/pages/_app.tsx
@@ -1,6 +1,15 @@
 import type { AppProps } from "next/app";
+import AuthProvider from "@/context/authcontext";
+import RedirectBasedOnAuth from "@/redirect/redirect";
+
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <RedirectBasedOnAuth>
+        <Component {...pageProps} />
+      </RedirectBasedOnAuth>
+    </AuthProvider>
+  )
 }
