3772abb08b1d73afd15fae7c534ddc7fa09c1770
s-alad <n7.saad@gmail.com>
2024-02-26 14:01:20 -0500

    fix up dashboard

--- app/op-next/pages/dashboard/index.tsx
+++ app/op-next/pages/dashboard/index.tsx
@@ -2,12 +2,6 @@ import { useState } from "react"
 import s from "./dashboard.module.scss"
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// Format of Class Data necessary for screen
-interface ClassData {
-    classID: string,
-    className: string,
-    owners: string[]
-}
 
 export default function Dashboard() {
     
@@ -15,11 +9,41 @@ export default function Dashboard() {
         <div className={s.dashboard}>
             <nav>
                 <div className={s.path}>Courses /</div>
-
                 <div className={s.person}>
                         <FontAwesomeIcon icon={faUser} />
                 </div>
             </nav>
+            <main className={s.main}>
+                <div className={s.classes}>
+                    <div className={s.class}>
+                        <div className={`${s.trap} ${s.yellow}`}></div>
+                        <div className={`${s.content} ${s.yellow}`}>
+                            <div className={s.info}>
+                                <div className={s.code}>XYZ123</div>
+                                <div className={s.name}>Introduction to Programming</div>
+                                <div className={s.teacher}>Prof. John Doe</div>
+                            </div>
+                            <div className={s.actions}>
+                                <div className={s.join}>join</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={s.class}>
+                        <div className={`${s.trap} ${s.pink}`}></div>
+                        <div className={`${s.content} ${s.pink}`}>
+                            <div className={s.info}>
+                                <div className={s.code}>ABC456</div>
+                                <div className={s.name}>Introduction to Bank Robbery</div>
+                                <div className={s.teacher}>Prof. Jane Doe</div>
+                            </div>
+                            <div className={s.actions}>
+                                <div className={s.join}>join</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }
