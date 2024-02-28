1bfb1bb4389569292a0a8a328f9c98f8d368bb3b
s-alad <n7.saad@gmail.com>
2024-02-28 17:03:19 -0500

    add to get ready for branches

---
+++ app/op-next/pages/create/class/index.tsx
@@ -0,0 +1,7 @@
+export default function CreateClass() {
+    return (
+        <div>
+            <h1>Create Class</h1>
+        </div>
+    )
+}
---
+++ app/op-next/pages/create/poll/index.tsx
@@ -0,0 +1,7 @@
+export default function CreatePoll() {
+    return (
+        <div>
+            <h1>Create Poll</h1>
+        </div>
+    )
+}
