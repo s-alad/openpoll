4ee1b5545f27e970cc129ec4e17a35fa5683c025
s-alad <n7.saad@gmail.com>
2024-02-28 17:19:35 -0500

    add sass

--- app/op-next/pages/create/class/index.tsx
+++ app/op-next/pages/create/class/index.tsx
@@ -1,6 +1,8 @@
 import Navbar from "@/layout/navbar/navbar"
 import React from "react"
 
+import styles from "./create.class.module.scss"
+
 export default function CreateClass() {
     return (
         <div>
