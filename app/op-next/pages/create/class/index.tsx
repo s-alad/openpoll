c641b376f1af95f014707971e127402b5221eb71
s-alad <n7.saad@gmail.com>
2024-02-28 17:23:00 -0500

    add fxn

--- app/op-next/pages/create/class/index.tsx
+++ app/op-next/pages/create/class/index.tsx
@@ -3,8 +3,22 @@ import React from "react"
 
 import styles from "./create.class.module.scss"
 
+import { User, getAdditionalUserInfo } from "firebase/auth";
+import { auth, db } from "../../../firebase/firebaseconfig";
+
+import { useRouter } from "next/router";
+
 export default function CreateClass() {
-    
+
+    const router = useRouter();
+
+
+    async function createclass() {
+        // in the document classes in firebase, create a new class
+        const user = auth.currentUser;
+        const uid = user!.uid;
+
+    }
 
     return (
         <div>
