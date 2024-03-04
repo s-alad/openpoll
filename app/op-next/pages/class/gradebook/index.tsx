c00300a85c11495c0a064ac0b3a79f136ec197a7
James <wong.james2003@gmail.com>
2024-03-04 15:12:10 -0500

    renamed to gardebook

--- app/op-next/pages/class/gradebook/index.tsx
+++ app/op-next/pages/class/gradebook/index.tsx
@@ -2,12 +2,14 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from 'react';
 import { collection, doc, getDocs } from 'firebase/firestore';
 import { db } from '@/firebase/firebaseconfig';
+import Poll from "@/models/poll";
 
 export default function analytics() {
     const router = useRouter();
     const classid = router.query.classid;
 
     const [students, setStudents] = useState([])
+    const [openpolls, setOpenpolls] = useState<Poll[]>([])
 
     async function getStudents() {
         // collection classes -> document ID -> collection students
@@ -18,7 +20,26 @@ export default function analytics() {
         
         // Grab the polls
         const pollsref = collection(classref, "polls")
+        
+        try {
+            const pollsSnapshot = await getDocs(pollsref);
+            const studentSnapshot = await getDocs(studentsref);
+            
+            // { pollsId : Answer }
+            let openpolls: { [key: string]: Poll };
+            pollsSnapshot.forEach((doc) => {
+                const pid = doc.id;
+                const data = doc.data() as Poll;
+                if (!data.classid) return;
+                console.log(pid, data);
+                openpolls[pid] = data
+            })
 
+            let studentAnswers: { [key: string]: [] }
+
+        } catch (e) {
+            console.error("Error getting documents: ", e);
+        }
         
     }
 
@@ -28,6 +49,7 @@ export default function analytics() {
             getStudents()
         }
     }, [classid])
+
     return(
         <div>{classid}</div>
     );
