6134574cac94db3f136bffd35641ea1de3193ddd
Tony Dao <tonydao@crc-dot1x-nat-10-239-126-3.bu.edu>
2024-03-23 17:35:06 -0400

    tried comparing grades

--- app/op-next/models/poll.ts
+++ app/op-next/models/poll.ts
@@ -8,12 +8,20 @@ interface Poll {
   answers: string[];
   created: any;
   active: boolean;
+<<<<<<< HEAD
   responses?: PollResponse;
   done?: boolean;
 }
 
 interface PollResponse {
   [option: string]: { [uid: string]: string };
+=======
+  responses: PollResponse;
+}
+
+interface PollResponse {
+  [option: string]: {};
+>>>>>>> afc921c (tried comparing grades)
 }
 
 export default Poll;
