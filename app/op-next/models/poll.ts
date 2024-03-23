359e1c40adc03d9f174b107e9ff5121b216760f0
Tony Dao <tonydao@crc-dot1x-nat-10-239-126-3.bu.edu>
2024-03-23 19:04:57 -0400

    displays poll question answer and response

--- app/op-next/models/poll.ts
+++ app/op-next/models/poll.ts
@@ -8,20 +8,13 @@ interface Poll {
   answers: string[];
   created: any;
   active: boolean;
-<<<<<<< HEAD
   responses?: PollResponse;
   done?: boolean;
 }
 
 interface PollResponse {
   [option: string]: { [uid: string]: string };
-=======
   responses: PollResponse;
-}
-
-interface PollResponse {
-  [option: string]: {};
->>>>>>> afc921c (tried comparing grades)
 }
 
 export default Poll;
