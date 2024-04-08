84be60520cd71d2a514b18a33eb11d9e587e967a
Tony Dao <tonydao@crc-dot1x-nat-10-239-126-3.bu.edu>
2024-04-08 13:19:16 -0400

    redid ui for Dashboard

--- app/op-next/pages/dashboard/[class]/index.tsx
+++ app/op-next/pages/dashboard/[class]/index.tsx
@@ -8,6 +8,7 @@ import { collection, doc, getDocs } from 'firebase/firestore';
 import { db } from '@/firebase/firebaseconfig';
 import Poll from '@/models/poll';
 import Loader from '@/components/loader/loader';
+import Image from 'next/image';
 
 interface PollAndId {
     poll: Poll;
@@ -23,7 +24,11 @@ export default function Dashboard() {
     const classid = router.query.class;
     console.log(classid);
 
+    type PollTypes = "Question" | "Attendance" | "Done"
+    const [polltype, setpolltype] = useState<PollTypes>("Question");
     const [openpolls, setOpenpolls] = useState<PollAndId[]>([]);
+    const [attendancePolls, setAttendancePolls] = useState<PollAndId[]>([]);
+    const [donePolls, setDonePolls] = useState<PollAndId[]>([]);
 
     async function getpolls() {
         setLoading(true);
@@ -35,20 +40,39 @@ export default function Dashboard() {
         try {
             const snapshot = await getDocs(pollsref);
             let openpolls: PollAndId[] = [];
+            let donepolls: PollAndId[] = [];
+            let attendancepolls: PollAndId[] = [];
             snapshot.forEach((doc) => {
                 const pid = doc.id;
                 const data = doc.data() as Poll;
-                if (!data.classid) return;
-                console.log(pid, data);
-                openpolls.push({ poll: data, id: pid })
+                
+                console.log(data.created, "date");
+                if (data.done) {
+                    donepolls.push({ poll: data, id: pid });
+                    return;
+                } else if (data.type === "attendance") {
+                    attendancepolls.push({ poll: data, id: pid });
+                } else {
+                    // Regular polls
+                    if (!data.classid) return;
+                    // console.log(pid, data, "poll");
+                    openpolls.push({ poll: data, id: pid })
+                }
+
+
             });
+            setAttendancePolls(attendancepolls);
+            setDonePolls(donepolls);
+            console.log(donepolls);
             setOpenpolls(openpolls);
+            console.log(openpolls);
         } catch (e) {
             console.error("Error getting documents: ", e);
         }
         
         setLoading(false);
     }
+
 
     //wait for router to load
     useEffect(() => {
@@ -58,20 +82,60 @@ export default function Dashboard() {
     }, [classid]);
 
     return (
-        <div className={s.class}>
-            {
-                loading ? <Loader/> :
-
-                    <div className={s.openpolls}>
+        <>
+            <main className={s.dashboard}>
+                <div className={s.header}>
+                    <div className={s.selector}>
                         {
+                            ["Question" , "Attendance" , "Done"].map(type => (
+                                <div
+                                    key={type}
+                                    onClick={() => setpolltype(type as PollTypes)}
+                                    className={polltype === type ? s.selected : ""}
+                                >
+                                    {type}
+                                </div>
+                            ))
+                        }
+                    </div>
+                    {
+                        polltype === "Question" && (
                             openpolls.map((poll, index) => {
                                 return (
                                     <div key={index} className={s.poll}>
                                         <div className={s.details}>
+                                            <div className={s.questContainer}>
+                                                <Image
+                                                    src="/question_answer.svg"
+                                                    alt="user"
+                                                    width={40}
+                                                    height={40}
+                                                />
+
+                                                <div className={s.question}>{poll.poll.question}</div>
+                                            </div>
+                                            <div className={s.date}>Date created: {new Date(poll.poll.created.seconds * 1000).toLocaleDateString()}</div>
+                                        </div>
+                                        <div className={s.actions}>
+                                            <button className={s.configure}>Configure</button>
+                                            <Link
+                                                href={{
+                                                    pathname: `/live/${classid}/${poll.id}`,
+                                                }}
+                                            ><button className={s.live}>Go Live</button></Link>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        )
+                    }
+                    {
+                        polltype === "Attendance" && (
+                            attendancePolls.map((poll, index) => {
+                                return (
+                                    <div key={index} className={s.poll}>
+                                        <div className={s.details}>
                                             <div className={s.question}>{poll.poll.question}</div>
-
-                                            {/* <div>created: {new Date(poll.poll.created.seconds).toLocaleDateString()}</div> */}
-
                                             <div>created: {new Date(poll.poll.created.seconds).toLocaleDateString()}</div>
                                         </div>
                                         <div className={s.actions}>
@@ -85,26 +149,53 @@ export default function Dashboard() {
                                     </div>
                                 )
                             })
-                        }
-                    </div>
-            }
+                        )
+                    }
+                    {
+                        polltype === "Done" && (
+                            donePolls.map((poll, index) => {
+                                return (
+                                    <div key={index} className={s.poll}>
+                                        <div className={s.details}>
+                                            <div className={s.questContainer}>
+                                                <Image
+                                                    src="/question_answer.svg"
+                                                    alt="user"
+                                                    width={40}
+                                                    height={40}
+                                                />
 
-            <div className={s.stalepolls}>
-
-            </div>
-
-            <div className={s.start}>
-                <Link
-                    href={{
-                        pathname: '/create/poll',
-                        query: { classid: router.query.class }
-                    }}
-                >
-                    <div className={s.add}>
-                        <FontAwesomeIcon icon={faPlus} />
-                    </div>
-                </Link>
-            </div>
-        </div>
-    )
+                                                <div className={s.question}>{poll.poll.question}</div>
+                                            </div>
+                                            <div className={s.date}>Date created: {new Date(poll.poll.created.seconds * 1000).toLocaleDateString()}</div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        )
+                    }
+                </div>
+                <div className={s.start}>
+                    <Link
+                        href={{
+                            pathname: '/create/poll',
+                            query: { classid: router.query.class }
+                        }}
+                    >
+                        <div className={s.add}>
+                            <FontAwesomeIcon icon={faPlus} />
+                        </div>
+                    </Link>
+                </div>
+                <div>
+                    <Image 
+                        src="/dashboard-bg.png"
+                        alt="logo"
+                        width={591}
+                        height={529}
+                    />
+                </div>
+            </main>
+        </>
+    );
 }
